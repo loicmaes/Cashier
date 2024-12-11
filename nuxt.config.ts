2185e4a7b88d8c04a91fd684be2ff27ffac91fd7
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-12-11 12:19:47 +0100

    :wrench: Configure project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  modules: [
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "shadcn-nuxt",
+    "nuxt-mailer",
+    "nuxt-scheduler",
+    "@nuxtjs/google-fonts",
+  ],
+  app: {
+    head: {
+      title: "Cashier, bienvenue",
+    },
+  },
+  devtools: { enabled: true },
+  colorMode: {
+    classSuffix: "",
+  },
+  compatibilityDate: "2024-11-01",
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: {
+        semi: true,
+        indent: 2,
+        quotes: "double",
+      },
+    },
+  },
+  googleFonts: {
+    families: {
+      Montserrat: true,
+    },
+  },
+  tailwindcss: {
+    cssPath: ["assets/css/tailwind.css", { injectPosition: "first" }],
+    configPath: "tailwind.config",
+    exposeConfig: false,
+    viewer: true,
+  },
+});
