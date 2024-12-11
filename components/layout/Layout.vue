88dfd1a20f6d5b81e20d2443f93b1b7bd4355723
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-12-11 13:14:40 +0100

    :rocket: Setup default components to layout app
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

---
+++ components/layout/Layout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { cn } from "~/lib/utils";
+
+interface LayoutProps {
+  name: string;
+  class?: HTMLAttributes["class"];
+}
+const props = defineProps<LayoutProps>();
+</script>
+
+<template>
+  <div
+    :data-layout="name"
+    :class="cn('', props.class)"
+  >
+    <slot />
+  </div>
+</template>
