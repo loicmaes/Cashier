db1629ff4bd5e0f41227a16405ac9e48b92eef0b
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-12-11 13:02:03 +0100

    :rocket: Add iconoir icon library
    Signed-off-by: loicmaes <97891718+loicmaes@users.noreply.github.com>

--- app.vue
+++ app.vue
@@ -1,6 +1,18 @@
+<script lang="ts" setup>
+import { IconoirProvider } from "@iconoir/vue";
+
+const iconProps = {
+  "width": "1.1rem",
+  "height": "1.1rem",
+  "stroke-width": "2",
+};
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <IconoirProvider :icon-props="iconProps">
+      <NuxtWelcome />
+    </IconoirProvider>
   </div>
 </template>
